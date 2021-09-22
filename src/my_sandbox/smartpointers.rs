1bf31057427115a78f219f317aed6bcebf4c8d4b
Mikiya Michishita <mmichish@mmichish.ubuntu20.04.kvm02.ssk.ynwm.yahoo.co.jp>
2021-09-22 23:50:12 +0900

    add Rc example

--- src/my_sandbox/smartpointers.rs
+++ src/my_sandbox/smartpointers.rs
@@ -1,3 +1,4 @@
+use std::rc::Rc;
 
 pub fn _vector_example() {
     // Box<T> locate data to heap
@@ -40,4 +41,45 @@ pub fn box_example() {
     println!("box = {:?}", list);
 }
 
+#[derive(Debug)]
+enum ListRc {
+    ConsRc(i32, Rc<ListRc>),
+    NilRc,
+}
+use ListRc::{ConsRc, NilRc};
 
+pub fn rc_example() {
+    // Rc<T> : smartpointer for reference counter
+    //         data is located in heap, and Rc<T> count it's onwer
+    // WARNING : Rc<T> will be used only in 'multi-thread' context
+    let a = Cons(5,
+        Box::new(Cons(10,
+            Box::new(Nil))));
+    // this wil be compile error, because 'a' is moved to 'b' and 'b' owns 'a'
+    // let b = Cons(3, Box::new(a));
+    // let c = Cons(4, Box::new(a));
+
+    // next, you will consider to obtain it's reference
+    let b_a = &a;
+    let c_a = &a;
+    println!("b_a : {:?}", b_a);
+    println!("c_a : {:?}", c_a);
+
+    // This code cannot compile due to mismatch type Cons(i32, Box<&List>) /= Cons(i32, Box<List>)
+    // let b = Cons(3, Box::new(b_a));
+    // let b = Cons(4, Box::new(c_a));
+
+    // you redefine ListRc, and use Rc::clone(&ref) to increment Rc reference counter and clone 'a'
+    // (but actualy don't copy 'a')
+    let a = Rc::new(ConsRc(5, Rc::new(ConsRc(10, Rc::new(NilRc)))));
+    println!("a (ref count) : {}", Rc::strong_count(&a));
+    let b = ConsRc(3, Rc::clone(&a));
+    let c = ConsRc(4, Rc::clone(&a));
+    println!("b (Rc) : {:?}", b);
+    println!("c (Rc) : {:?}", c);
+    println!("a (ref count) : {}", Rc::strong_count(&a));
+}
+
+
+pub fn refcall_example() {
+}
